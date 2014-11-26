c01937cbe2c67c68873f8fe3649d8ce69d7877c3
OwenMa1 <owmo1@hotmail.com>
2014-11-26 15:18:41 +0100

    Basis klaar

--- app/src/main/java/nl/qntn/tictacto/tictactoe/MyActivity.java
+++ app/src/main/java/nl/qntn/tictacto/tictactoe/MyActivity.java
@@ -6,56 +6,87 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
 import android.view.View;
+import android.widget.Toast;
+
+import java.util.Random;
 
 
 public class MyActivity extends Activity {
 
     Button clickedButton;
+    Button[] allButtons = new Button[9];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my);
 
+        final Button button1 = (Button) findViewById(R.id.button1);
+        final Button button2 = (Button) findViewById(R.id.button2);
+        final Button button3 = (Button) findViewById(R.id.button3);
+        final Button button4 = (Button) findViewById(R.id.button4);
+        final Button button5 = (Button) findViewById(R.id.button5);
+        final Button button6 = (Button) findViewById(R.id.button6);
+        final Button button7 = (Button) findViewById(R.id.button7);
+        final Button button8 = (Button) findViewById(R.id.button8);
+        final Button button9 = (Button) findViewById(R.id.button9);
+
+        allButtons[0] = button1;
+        allButtons[1] = button2;
+        allButtons[2] = button3;
+        allButtons[3] = button4;
+        allButtons[4] = button5;
+        allButtons[5] = button6;
+        allButtons[6] = button7;
+        allButtons[7] = button8;
+        allButtons[8] = button9;
+
     }
 
-        //button.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
+    //button.setOnClickListener(new View.OnClickListener() {
+    public void onClick(View v) {
 
-                clickedButton = (Button) findViewById(v.getId());
+        clickedButton = (Button) findViewById(v.getId());
 
-                // Perform action on click
-                switch (v.getId()) {
-                    case R.id.button1:
-                        clickedButton.setText("x");
-                        break;
-                    case R.id.button2:
-                        clickedButton.setText("x");
-                        break;
-                    case R.id.button3:
-                        clickedButton.setText("x");
-                        break;
-                    case R.id.button4:
-                        clickedButton.setText("x");
-                        break;
-                    case R.id.button5:
-                        clickedButton.setText("x");
-                        break;
-                    case R.id.button6:
-                        clickedButton.setText("x");
-                        break;
-                    case R.id.button7:
-                        clickedButton.setText("x");
-                        break;
-                    case R.id.button8:
-                        clickedButton.setText("x");
-                        break;
-                    case R.id.button9:
-                        clickedButton.setText("x");
-                        break;
-                }
+        clickedButton.setText("x");
+
+        if (checkIfWin()) {
+            Toast.makeText(getApplicationContext(),"You won! Baas!",Toast.LENGTH_LONG).show();
+        } else {
+            computersTurn();
+        }
+
+    }
+
+public boolean checkIfWin() {
+
+    // links recht check
+    if (allButtons[0].getText() == "x" && allButtons[1].getText() == "x" && allButtons[2].getText() == "x" )  {
+        return true;
+    }
+    return false;
+
+}
+
+
+
+
+
+    // Computer gaat iets doen.
+    public void computersTurn() {
+
+        boolean computerZet = true;
+
+        while(computerZet) {
+            Random r = new Random();
+            int randGetal = r.nextInt(9- 0) + 0;
+
+            if (allButtons[randGetal].getText() != "x" && allButtons[randGetal].getText() != "o") {
+                allButtons[randGetal].setText("o");
+                computerZet = false;
             }
-        //}
+        }
+    }
 
     // tijdelijk oplossing later natuurlijk beter...
     public void onClickReset(View v){
